076de4d804b2b414687b3a40c48521a3b95939a0
Nathanael Morris <nathanaelmorris15@gmail.com>
2019-04-05 22:47:47 -0500

    First commit

--- NegativityIndex.java
+++ NegativityIndex.java
@@ -9,40 +9,51 @@ public class NegativityIndex {
 
         ConfigurationBuilder cb = new ConfigurationBuilder();
         cb.setDebugEnabled(true)
-        .setOAuthConsumerKey("")
-        .setOAuthConsumerSecret("")
-        .setOAuthAccessToken("")
-        .setOAuthAccessTokenSecret("");
+        .setOAuthConsumerKey("TZ2ojnd1SUeBZoPGTB48FsR0s")
+        .setOAuthConsumerSecret("lLONlfKbPKcgOwH856b2dMZVpfvjUnuDmaZNXREbygakCsjhS8")
+        .setOAuthAccessToken("1114320820581675008-1EEqAIU9sFdAVljlWv7sRzyOWZFlfc")
+        .setOAuthAccessTokenSecret("twSM2V7avNgGv5zER3e4qGm5G2BDHJxx9ljqVSwzFQOb0");
         TwitterFactory tf = new TwitterFactory(cb.build());
         Twitter twitter = tf.getInstance();
+        ArrayList<String> str = new ArrayList<String>();
 		try {
-			 ResponseList<User> statuses;
+			 ResponseList<Status> statuses;
 			 Scanner input = new Scanner(System.in);
 			 System.out.println("Enter Twitter Screen Name: ");
 			 String user = input.nextLine();
-			  
+			 
+			 twitter.createFriendship(user);
+			 
 			 if (args.length == 1)
 			 {
 				user = args[0];
-				statuses = twitter.searchUsers(user, 1);
+					statuses = twitter.getHomeTimeline();
 			 } 
 			 else 
 			 {
 				 user = twitter.verifyCredentials().getScreenName();
-				 statuses = twitter.searchUsers(user, 1);
+				 statuses = twitter.getHomeTimeline();
 			 }
 			
+			 String error = twitter.destroyFriendship(user);
+			 
 			 System.out.println("Showing @" + user + "'s user timeline.");
-			 for (User status : statuses) {
-				// System.out.println("@" + status.getUser().getScreenName() + " - " + status.getText());
-				 System.out.println(status.getStatus() + "*");
+			 for (Status status : statuses) {
+				System.out.println("@" + status.getUser().getScreenName() + " - " + status.getText());
+				str.add(status.getText());
+				 //System.out.println(status.getStatus() + "*");
 			 }
 		} catch (TwitterException te) 
 		{
 				 
+
 			te.printStackTrace();
 			System.out.println("Failed to get timeline: " + te.getMessage());
 			System.exit(-1);
+		}
+		for(int i = 0; i < str.size(); i++)
+		{
+			System.out.println(str.get(i));
 		}
     }
 }
